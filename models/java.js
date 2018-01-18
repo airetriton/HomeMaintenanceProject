cda81f2f3a639ef9f759fc58f806b16cc13e2870
Brady <brady.withers@outlook.com>
2018-01-17 18:54:16 -0700

    larger checkboxes

--- models/java.js
+++ models/java.js
@@ -1,23 +1,4 @@
 $(document).ready(function(){
 
-//     var click = document.createElement("audio");
-//     click.setAttribute("src", "../public/sounds/click.wav");
-
-
-// //var url="preventative.html"
-
-
-// $(".click").on("click", function(){
-// console.log("i just got clicked");
-// function delay (url) { 
-//     console.log("hello 1");
-//     setTimeout( click.play(), 500 );
-//     console.log("hello2");
-// }
-// delay("prevntative.html");
-    
-// });
-
-
     $('[data-toggle="popover"]').popover();   
  });
